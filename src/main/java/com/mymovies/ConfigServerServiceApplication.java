b9b7681096d6faa850327c583fb931c842a17a00
Ranushan Rachu <ranusha.n@hotmail.fr>
2020-04-30 14:20:56 +0200

    [ORIGIN][MASTER][ADDED] dependency file, source code and ignoring file

---
+++ src/main/java/com/mymovies/ConfigServerServiceApplication.java
@@ -0,0 +1,22 @@
+package com.mymovies;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableDiscoveryClient
+public class ConfigServerServiceApplication {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ConfigServerServiceApplication.class);
+
+    public static void main(String[] args) {
+        LOGGER.info("Start Service Config Server");
+        SpringApplication.run(ConfigServerServiceApplication.class, args);
+    }
+
+}
