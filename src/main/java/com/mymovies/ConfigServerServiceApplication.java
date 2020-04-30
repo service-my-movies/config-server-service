d1cca135fb8d9926952cf7905e83687fdc52ebf3
Ranushan <ranusha.n@hotmail.fr>
2020-04-30 20:47:57 +0200

    [ORIGIN][MASTER][UPDATED] Add RefreshScope

--- src/main/java/com/mymovies/ConfigServerServiceApplication.java
+++ src/main/java/com/mymovies/ConfigServerServiceApplication.java
@@ -6,10 +6,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
 @EnableConfigServer
 @EnableDiscoveryClient
+@RefreshScope
 public class ConfigServerServiceApplication {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ConfigServerServiceApplication.class);
